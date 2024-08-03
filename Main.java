2b8ab635bd55d82e325cf9d92a97362405964339
kevincord <85372037+kevincord@users.noreply.github.com>
2024-08-02 22:58:05 -0600

    Versión 4: Mejorar función de saludo para manejar nombres vacíos

--- Main.java
+++ Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         greet("Kevin");
+        greet("");
     }
 
     public static void greet(String name) {
-        System.out.println("Hola, " + name + "!");
+        if (name.isEmpty()) {
+            System.out.println("Hola, Estraño!");
+        } else {
+            System.out.println("Hello, " + name + "!");
+        }
     }
 }
